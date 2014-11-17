c7316cfd520a90b617dfb41bd8ad62f9d8918927
Clinic <clinic@ClinicdeMacBook-Pro.local>
2014-11-17 17:05:06 +0800

    first

---
+++ M3U8Player/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  M3U8Player
+//
+//  Created by Clinic on 14-11-17.
+//  Copyright (c) 2014年 Clinic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ M3U8PlayerTests/M3U8PlayerTests.swift
@@ -0,0 +1,36 @@
+//
+//  M3U8PlayerTests.swift
+//  M3U8PlayerTests
+//
+//  Created by Clinic on 14-11-17.
+//  Copyright (c) 2014年 Clinic. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class M3U8PlayerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
