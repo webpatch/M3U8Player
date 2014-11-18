f80ef2d1659e3d8fddf189958f7b3f659842986e
Kiny <kiny@KinydeMac-Pro.local>
2014-11-18 21:55:41 +0800

    t

---
+++ M3U8Player/Header.h
@@ -0,0 +1,8 @@
+//
+//  Header.h
+//  M3U8Player
+//
+//  Created by Kiny on 14-11-18.
+//  Copyright (c) 2014年 Clinic. All rights reserved.
+//
+#import "AFNetworking.h"
--- M3U8Player/ViewController.swift
+++ M3U8Player/ViewController.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-import KinyLib
 
 class ViewController: UIViewController {
 
@@ -16,16 +15,52 @@ class ViewController: UIViewController {
         let p = NSBundle.mainBundle().pathForResource("m3u8", ofType: "txt")!
         let m3u8 = NSString(contentsOfFile: p, encoding: NSUTF8StringEncoding, error: nil)!
         let arr = m3u8.componentsSeparatedByCharactersInSet(NSCharacterSet.newlineCharacterSet()) as [String]
-        var videoURLs = [String]()
+        var videoURLs = [NSURL]()
         for str:String in arr
         {
             if str.hasPrefix("http://")
             {
                 let url:String = (str as NSString).componentsSeparatedByString("?")[0] as String
                 println(url)
-                videoURLs.append(url)
+                videoURLs.append(NSURL(string: url)!)
             }
         }
+        
+        let vp = NSTemporaryDirectory()
+        println(vp)
+        var opArr = NSMutableArray()
+        var num = 0
+        for url in videoURLs
+        {
+            let r =  NSURLRequest(URL: url)
+            var op = AFHTTPRequestOperation(request:r)
+//            op.responseSerializer = AFImageResponseSerializer
+//            op.outputStream = NSOutputStream(toFileAtPath: NSTemporaryDirectory(), append: false)
+            op.setDownloadProgressBlock({ (byetsRead, totalBytesRead, totalBytesExpectedToRead) -> Void in
+                println(Double(totalBytesRead)/Double(totalBytesExpectedToRead))
+            })
+            op.setCompletionBlockWithSuccess({(AFHTTPRequestOperation, AnyObject) -> Void in
+//                println(AFHTTPRequestOperation.responseData)
+                var data:NSData = AFHTTPRequestOperation.responseData
+                println("\(vp)\(num)")
+                data.writeToFile("\(vp)\\\(num)", atomically: true)
+                
+            }, failure: { (AFHTTPRequestOperation, NSError) -> Void in
+               
+            })
+            opArr.addObject(op)
+            num+=1
+        }
+        
+        var batches = AFURLConnectionOperation.batchOfRequestOperations(opArr, progressBlock: { (numberOfFinishedOperations, totalNumberOfOperations) -> Void in
+            println(numberOfFinishedOperations)
+        },completionBlock:{ (operations) -> Void in
+            println("zzzzzz")
+        })
+        
+        
+        
+        NSOperationQueue.mainQueue().addOperations(batches, waitUntilFinished: false)
         
         println("total \(videoURLs.count)")
         // Do any additional setup after loading the view, typically from a nib.
