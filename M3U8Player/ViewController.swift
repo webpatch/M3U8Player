0f1f7a2b01f3b4bf7e6852a314069e3867c8500b
Clinic <clinic@ClinicdeMacBook-Pro.local>
2014-11-18 18:01:06 +0800

    go

--- M3U8Player/ViewController.swift
+++ M3U8Player/ViewController.swift
@@ -7,11 +7,27 @@
 //
 
 import UIKit
+import KinyLib
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let p = NSBundle.mainBundle().pathForResource("m3u8", ofType: "txt")!
+        let m3u8 = NSString(contentsOfFile: p, encoding: NSUTF8StringEncoding, error: nil)!
+        let arr = m3u8.componentsSeparatedByCharactersInSet(NSCharacterSet.newlineCharacterSet()) as [String]
+        var videoURLs = [String]()
+        for str:String in arr
+        {
+            if str.hasPrefix("http://")
+            {
+                let url:String = (str as NSString).componentsSeparatedByString("?")[0] as String
+                println(url)
+                videoURLs.append(url)
+            }
+        }
+        
+        println("total \(videoURLs.count)")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
